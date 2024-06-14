af29b9a922bcdf69c631153732a264738fbc6c53
Antonioafj <antonioafj.edu@gmail.com>
2024-06-13 23:07:23 -0400

    arvore-binaria-busca

---
+++ src/main/java/dataStructure/Main.java
@@ -0,0 +1,20 @@
+package dataStructure;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST tree = new BST();
+
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
+        tree.insert(8);
+        tree.insert(17);
+        tree.insert(42);
+        tree.insert(72);
+
+        tree.deleteNode(8);
+        tree.inOrder();
+    }
+}
